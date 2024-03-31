4d2f868aa9ab2de647daa81c0e3e27f1ade3e13f
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-03-31 16:44:51 +0900

    :wrench: @nestjs/cli 재설치 및 PORT Logger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 3000;
+  await app.listen(PORT);
+  Logger.log(
+    `${PORT}번 포트로 서버 실행 중... API Docs: http://localhost:3000/api`,
+  );
 }
 bootstrap();
