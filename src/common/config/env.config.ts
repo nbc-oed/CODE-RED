dae919e4561a20d6f98ee56e2363ca705ac9a9b3
ooheunda <ooheunda@gmail.com>
2024-04-01 21:15:14 +0900

    :recycle: env validation schema 파일 추가

---
+++ src/common/config/env.config.ts
@@ -0,0 +1,34 @@
+import Joi from 'joi';
+
+const dbSchema = {
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_NAME: Joi.string().required(),
+  DB_SYNC: Joi.boolean().required(),
+};
+
+const jwtSchema = {
+  JWT_SECRET_KEY: Joi.string().required(),
+};
+
+const redisSchema = {
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
+  REDIS_PASSWORD: Joi.string().required(),
+};
+
+const awsSchema = {
+  AWS_REGION: Joi.string().required(),
+  AWS_S3_ACCESS_KEY_ID: Joi.string().required(),
+  AWS_S3_SECRET_ACCESS_KEY: Joi.string().required(),
+  AWS_S3_BUCKET_NAME: Joi.string().required(),
+};
+
+export const validationSchema = Joi.object({
+  ...dbSchema,
+  ...jwtSchema,
+  ...redisSchema,
+  ...awsSchema,
+});
