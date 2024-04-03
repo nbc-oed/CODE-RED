b9b1a037f255001f4833e417eeda37acf0243f25
startcoriny <gjm0313@naver.com>
2024-04-03 17:06:33 +0900

    :wrench: add news Entity

---
+++ src/news/entities/news.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'news' })
+export class News {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  url: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  media: string;
+
+  @CreateDateColumn({ type: 'date', nullable: false })
+  created_at: Date;
+}
