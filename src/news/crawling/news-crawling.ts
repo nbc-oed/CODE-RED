44a75efd026f7f1aa3ccb750a543c3663712529c
startcoriny <gjm0313@naver.com>
2024-04-04 11:06:08 +0900

    :recycle: Refactor duplicate code & delete Conditions for crawling when no Show More button

--- src/news/crawling/news-crawling.ts
+++ src/news/crawling/news-crawling.ts
@@ -8,29 +8,33 @@ export class Crawling {
     });
     try {
       const page = await browser.newPage();
-
       page.setDefaultNavigationTimeout(2 * 60 * 1000);
-
       await Promise.all([
         await page.goto('https://news.naver.com/breakingnews/section/102/249'),
         await page.click('._CALENDAR_LAYER_TOGGLE'),
         // await page.click(`.calendar .calendar-day-2024-04-01`),
         await page.click(`.calendar .calendar-day-${today()}`),
       ]);
-
       await clickMorePageEnd(page);
-
       const result = await page.$$eval(
-        '.section_latest_article .section_article >ul >li .sa_item_inner .sa_item_flex .sa_text',
+        '.section_article .sa_item',
         (resultItems) => {
           return resultItems
-            .filter((resultItem) => {
-              const title = resultItem.querySelector('a').innerText;
+            .map((resultItem) => {
+              const titleElement = resultItem
+                .querySelector('.sa_text')
+                .querySelector('a');
               const textElement = resultItem.querySelector('.sa_text_lede');
+              const newsCompanyElement =
+                resultItem.querySelector('.sa_text_press');
+
+              const title = titleElement ? titleElement.innerText.trim() : '';
+              const url = titleElement ? titleElement.href : '';
               const text = textElement ? textElement.textContent.trim() : '';
-              return title.includes('서울') || text.includes('서울');
-            })
-            .filter((resultItem) => {
+              const newsCompany = newsCompanyElement
+                ? newsCompanyElement.textContent.trim()
+                : '';
+
               const keywords = [
                 '사고',
                 '폭행',
@@ -47,21 +51,18 @@ export class Crawling {
                 '칼부림',
                 '묻지마',
               ];
-              const title = resultItem.querySelector('a').innerText;
-              return keywords.some((keyword) => title.includes(keyword));
-            })
-            .map((resultItem) => {
-              const titleElement = resultItem.querySelector('a');
-              const newsCompanyElement =
-                resultItem.querySelector('.sa_text_press');
 
-              const url = titleElement.href;
-              const title = titleElement.innerText;
-              const newsCompany = newsCompanyElement
-                ? newsCompanyElement.textContent.trim()
-                : '';
+              if (!(title.includes('서울') || text.includes('서울'))) {
+                return null;
+              }
+
+              if (!keywords.some((keyword) => title.includes(keyword))) {
+                return null;
+              }
+
               return { url, title, newsCompany };
-            });
+            })
+            .filter((item) => item !== null);
         },
       );
 
@@ -78,6 +79,7 @@ export class Crawling {
           }
         }
       }
+
       return result;
     } finally {
       await browser.close();
@@ -87,15 +89,12 @@ export class Crawling {
 
 const today = () => {
   const today = new Date();
-
   const year = today.getFullYear();
   const month = today.getMonth() + 1;
   const strMonth = month < 10 ? '0' + month : month;
   const day = today.getDate();
   const strDay = day < 10 ? '0' + day : day;
-
   const formattedDate = `${year}-${strMonth}-${strDay}`;
-
   return formattedDate;
 };
 
@@ -110,12 +109,8 @@ async function clickMorePageEnd(page) {
     const endpoint = await page.$(
       '.section_latest .section_more a[style="display: none;"]',
     );
+    if (endpoint) break;
 
-    const newsDiv = await page.$$(
-      '.section_latest .section_latest_article .section_article ',
-    );
-
-    if (newsDiv.length < 7) break;
     if (endpoint) break;
     await moreButton.evaluate((b) => b.click());
   }
