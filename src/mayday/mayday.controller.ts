4ac9c6dcbb7fb28bdbb579109aff15bd28c90490
startcoriny <gjm0313@naver.com>
2024-04-09 05:03:30 +0900

    :sparkles: new feature: save my location

--- src/mayday/mayday.controller.ts
+++ src/mayday/mayday.controller.ts
@@ -1,17 +1,28 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Render, UseGuards } from '@nestjs/common';
 import { MaydayService } from './mayday.service';
-import { CreateMaydayDto } from './dto/create-mayday.dto';
-import { UpdateMaydayDto } from './dto/update-mayday.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { UserInfo } from 'src/common/decorator/user.decorator';
+import { Users } from 'src/common/entities/users.entity';
+import { CreateLocationDto } from './dto/create-location.dto';
 
 @Controller('mayday')
+@UseGuards(AuthGuard('jwt'))
 export class MaydayController {
   constructor(private readonly maydayService: MaydayService) {}
+
+  // 지울것
+  @Get()
+  @Render('index')
+  main() {
+    return { title: '내 위치 정보 저장' };
+  }
+
+  // 내위치 정보 저장
+  @Post()
+  async saveMyLocation(
+    @UserInfo() user: Users,
+    @Body() createLocationDto: CreateLocationDto,
+  ) {
+    await this.maydayService.saveMyLocation(createLocationDto, user.id);
+  }
 }
--- src/mayday/mayday.service.ts
+++ src/mayday/mayday.service.ts
@@ -1,6 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMaydayDto } from './dto/create-mayday.dto';
-import { UpdateMaydayDto } from './dto/update-mayday.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
+import { Repository } from 'typeorm';
+import { CreateLocationDto } from './dto/create-location.dto';
 
 @Injectable()
-export class MaydayService {}
+export class MaydayService {
+  constructor(
+    @InjectRepository(Location)
+    private readonly locationRepository: Repository<Location>,
+  ) {}
+
+  // 내위치 정보 저장
+  async saveMyLocation(createLocationDto: CreateLocationDto, userId: number) {
+    const { latitude, longitude } = createLocationDto;
+
+    const user = await this.locationRepository
+      .createQueryBuilder()
+      .select()
+      .where('user_id = :userId', { userId: userId })
+      .getOne();
+
+    if (user) {
+      await this.locationRepository
+        .createQueryBuilder()
+        .update()
+        .set({
+          latitude: latitude,
+          longitude: longitude,
+          location: () => `ST_GeomFromText('POINT(${longitude} ${latitude})')`,
+        })
+        .where('user_id = :userId', { userId: userId })
+        .execute();
+    } else {
+      await this.locationRepository
+        .createQueryBuilder()
+        .insert()
+        .into('location')
+        .values({
+          user_id: userId,
+          latitude: latitude,
+          longitude: longitude,
+          location: () => `ST_GeomFromText('POINT(${longitude} ${latitude})')`,
+        })
+        .execute();
+    }
+  }
+}
