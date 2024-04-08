f06e4bc99e12a03307716671af84528c034f68f2
8860k <ysmkk@naver.com>
2024-04-08 20:50:22 +0900

    :sparkles: kakao login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService, KakaoLogin } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
@@ -8,9 +8,12 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/common/entities/users.entity';
 import { AwsModule } from 'src/aws/aws.module';
-
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    HttpModule,
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     JwtModule.registerAsync({
       useFactory: (config: ConfigService) => ({
@@ -23,6 +26,6 @@ import { AwsModule } from 'src/aws/aws.module';
     AwsModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, KakaoLogin],
 })
 export class AuthModule {}
