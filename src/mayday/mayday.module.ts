3c66dc1cd4cfc6ae6dd06844a910f2c71bfc33e4
startcoriny <gjm0313@naver.com>
2024-04-09 04:58:31 +0900

    :heavy_plus_sign: add module typeOrmModule, add Location entity

--- src/mayday/mayday.module.ts
+++ src/mayday/mayday.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MaydayService } from './mayday.service';
 import { MaydayController } from './mayday.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Location])],
   controllers: [MaydayController],
   providers: [MaydayService],
 })
