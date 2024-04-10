b5bbd17ff4b4fc27e1ce7d292ebe3ff199072ae4
unknown <mgeun97@naver.com>
2024-04-11 01:45:44 +0900

    :sparkles: : new feature. real-time destination risk data. but incomplete..! temporarily.

--- src/destination-risk/destination-risk.controller.ts
+++ src/destination-risk/destination-risk.controller.ts
@@ -1,9 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { DestinationRiskService } from './destination-risk.service';
 
 @Controller('destination-risk')
 export class DestinationRiskController {
     constructor (private readonly destinationRiskService : DestinationRiskService) {}
 
-    
+    @Get('get')
+    async getDestinationRisk (@Query('destination') destination : string) {
+        const destinationRisk = await this.destinationRiskService.getDestinationRisk(destination)
+        return destinationRisk
+    }
 }
--- src/destination-risk/destination-risk.service.ts
+++ src/destination-risk/destination-risk.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import convert from 'xml-js'
 
 @Injectable()
-export class DestinationRiskService {}
+export class DestinationRiskService {
+    constructor (
+        private configService: ConfigService,
+    ) {}
+
+    async getDestinationRisk (destination : string) {
+        const seoulRealTimeData = await this.configService.get('REAL_TIME_DATA_API')
+        const response = await axios.get(`http://openapi.seoul.go.kr:8088/${seoulRealTimeData}/xml/citydata/1/5/${destination}`)
+        const xmlData = response.data
+        const xmlToJsonData = convert.xml2json(xmlData)
+        const realTimeDataJsonVer = JSON.parse(xmlToJsonData)
+        const areaName = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']
+        const areaCongestLvlMsg = realTimeDataJsonVer['SeoulRtd.citydat']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']
+        const realTimeDestinationRiskData = {
+             areaName,
+             areaCongestLevel : areaCongestLvlMsg.AREA_CONGEST_LVL,
+             areaCongestMessage : areaCongestLvlMsg.AREA_CONGEST_MSG
+        }
+        return realTimeDestinationRiskData
+    }
+}
