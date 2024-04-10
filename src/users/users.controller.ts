cb82a6721564d531f68322e07a718b62d65ad1f7
startcoriny <gjm0313@naver.com>
2024-04-11 02:47:28 +0900

    :memo: modify log

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,46 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Users } from 'src/common/entities/users.entity';
-import { UserInfo } from 'src/common/decorator/user.decorator';
 import { AuthGuard } from '@nestjs/passport';
+import { UserInfo } from 'src/common/decorator/user.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-    // 모든 유저 조회
+  // 모든 유저 조회
   @Get()
-  getUsers(){
+  getUsers() {
     return this.usersService.getAllUsers();
   }
 
   // 유저 상세 조회
-  @Get(":id")
-  async getOneUsers(
-    @Param('id') id: number) {
+  @Get(':id')
+  async getOneUsers(@Param('id') id: number) {
     return await this.usersService.findOne(id);
   }
 
+  // 유저 수정
+  @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
+  async update(
+    @Param('id') id: number,
+    @UserInfo() user: Users,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.usersService.update(id, user, updateUserDto);
+  }
 
-  // 유저 수정 
-    @Patch(':id')
-    @UseGuards(AuthGuard('jwt'))
-    async update(
-      @Param('id') id : number,
-      @UserInfo() user:Users,
-      @Body() updateUserDto: UpdateUserDto) 
-      {
-      return await this.usersService.update(id,user, updateUserDto);
-    }
-  
-    //유저 삭제
-    @Delete(':id')
-    async remove(
-      @Param('id') id : number, 
-      @UserInfo() user : Users)
-      {
-      return await this.usersService.remove(id, user);
-    }
+  //유저 삭제
+  @Delete(':id')
+  async remove(@Param('id') id: number, @UserInfo() user: Users) {
+    return await this.usersService.remove(id, user);
+  }
 }
