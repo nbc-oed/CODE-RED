309c1798b96899ea50c63b9f828542dd7926b42f
unknown <mgeun97@naver.com>
2024-04-11 21:08:33 +0900

    :zap: : detail~

--- src/destination-risk/destination-risk.service.ts
+++ src/destination-risk/destination-risk.service.ts
@@ -23,15 +23,15 @@ export class DestinationRiskService {
 
         // 인구 추이는 가장 최근 시간대 기준(12:30이면 13시 기준)
         // 임의로 예측 실시간 인구 지표 최소값과 예측 실시간 인구 지표 최대값의 평균 값으로 나타내었음
-        // '인구 추이' : `${realTimeDataJsonVer.FCST_TIME._text}기준 약 ${predictedPopulation}명` 으로 변경해서 기준 시간대 표현도 생각해봄직함
+        // '인구 추이' : `${populationTrends.FCST_TIME._text}기준 약 ${predictedPopulation}명` 으로 변경해서 기준 시간대 표현도 생각해봄직함
         const populationTrends =
         realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']['FCST_PPLTN']['FCST_PPLTN'][0]
         const predictedPopulation = (parseInt(populationTrends.FCST_PPLTN_MIN._text) + parseInt(populationTrends.FCST_PPLTN_MAX._text)) /2
         const realTimeDestinationRiskData = {
              '목적지' : areaName,
-             '장소 혼잡도' : areaCongestLvlMsg.AREA_CONGEST_LVL._text,
+             '실시간 장소 혼잡도' : areaCongestLvlMsg.AREA_CONGEST_LVL._text,
              '관련 안내사항' : areaCongestLvlMsg.AREA_CONGEST_MSG._text,
-             '인구 추이' : `약 ${predictedPopulation}명`
+             '예상 인구' : `약 ${predictedPopulation}명, ${populationTrends.FCST_TIME._text}기준`
         }
         return realTimeDestinationRiskData
     }
