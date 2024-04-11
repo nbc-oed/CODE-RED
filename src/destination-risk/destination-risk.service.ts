3e293936d7efffc8dcea822889a3b23c6621cddf
unknown <mgeun97@naver.com>
2024-04-11 12:44:54 +0900

    :sparkles: : add function - predictedPopulation

--- src/destination-risk/destination-risk.service.ts
+++ src/destination-risk/destination-risk.service.ts
@@ -20,10 +20,15 @@ export class DestinationRiskService {
         const realTimeDataJsonVer = JSON.parse(xmlToJsonData)
         const areaName = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['AREA_NM']['_text']
         const areaCongestLvlMsg = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']
+        // 인구 추이는 임의로 예측 실시간 인구 지표 최소값과 예측 실시간 인구 지표 최대값의 평균 값으로 나타내었음
+        const populationTrends =
+        realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']['FCST_PPLTN']['FCST_PPLTN'][0]
+        const predictedPopulation = (parseInt(populationTrends.FCST_PPLTN_MIN._text) + parseInt(populationTrends.FCST_PPLTN_MAX._text)) /2
         const realTimeDestinationRiskData = {
              '목적지' : areaName,
              '장소 혼잡도' : areaCongestLvlMsg.AREA_CONGEST_LVL._text,
-             '관련 안내사항' : areaCongestLvlMsg.AREA_CONGEST_MSG._text
+             '관련 안내사항' : areaCongestLvlMsg.AREA_CONGEST_MSG._text,
+             '인구 추이' : `약 ${predictedPopulation}명`
         }
         return realTimeDestinationRiskData
     }
