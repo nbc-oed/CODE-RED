af774ea617bf0a574a247e07933d4d17ab5aeedb
unknown <mgeun97@naver.com>
2024-04-11 13:03:56 +0900

    :bulb: : add comment

--- src/destination-risk/destination-risk.service.ts
+++ src/destination-risk/destination-risk.service.ts
@@ -20,7 +20,10 @@ export class DestinationRiskService {
         const realTimeDataJsonVer = JSON.parse(xmlToJsonData)
         const areaName = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['AREA_NM']['_text']
         const areaCongestLvlMsg = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']
-        // 인구 추이는 가장 최근 시간대 기준(12:30이면 13시 기준) 임의로 예측 실시간 인구 지표 최소값과 예측 실시간 인구 지표 최대값의 평균 값으로 나타내었음
+
+        // 인구 추이는 가장 최근 시간대 기준(12:30이면 13시 기준)
+        // 임의로 예측 실시간 인구 지표 최소값과 예측 실시간 인구 지표 최대값의 평균 값으로 나타내었음
+        // '인구 추이' : `${realTimeDataJsonVer.FCST_TIME._text}기준 약 ${predictedPopulation}명` 으로 변경해서 기준 시간대 표현도 생각해봄직함
         const populationTrends =
         realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']['FCST_PPLTN']['FCST_PPLTN'][0]
         const predictedPopulation = (parseInt(populationTrends.FCST_PPLTN_MIN._text) + parseInt(populationTrends.FCST_PPLTN_MAX._text)) /2
