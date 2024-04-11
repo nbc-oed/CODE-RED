96e05d18c47c7d9ac4c0afcb06dc11fd0ae5593b
unknown <mgeun97@naver.com>
2024-04-11 11:45:00 +0900

    :zap: : destination-risk check function complete. non-GPS

--- src/destination-risk/destination-risk.service.ts
+++ src/destination-risk/destination-risk.service.ts
@@ -11,16 +11,19 @@ export class DestinationRiskService {
 
     async getDestinationRisk (destination : string) {
         const seoulRealTimeData = await this.configService.get('REAL_TIME_DATA_API')
-        const response = await axios.get(`http://openapi.seoul.go.kr:8088/${seoulRealTimeData}/xml/citydata/1/5/${destination}`)
+        const response = await axios.get(`http://openapi.seoul.go.kr:8088/${seoulRealTimeData}/xml/citydata/1/1/${destination}`)
         const xmlData = response.data
-        const xmlToJsonData = convert.xml2json(xmlData)
+        const xmlToJsonData = convert.xml2json(xmlData, {
+            compact : true,
+            spaces : 4
+        })
         const realTimeDataJsonVer = JSON.parse(xmlToJsonData)
-        const areaName = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']
-        const areaCongestLvlMsg = realTimeDataJsonVer['SeoulRtd.citydat']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']
+        const areaName = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['AREA_NM']['_text']
+        const areaCongestLvlMsg = realTimeDataJsonVer['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']
         const realTimeDestinationRiskData = {
-             areaName,
-             areaCongestLevel : areaCongestLvlMsg.AREA_CONGEST_LVL,
-             areaCongestMessage : areaCongestLvlMsg.AREA_CONGEST_MSG
+             '목적지' : areaName,
+             '장소 혼잡도' : areaCongestLvlMsg.AREA_CONGEST_LVL._text,
+             '관련 안내사항' : areaCongestLvlMsg.AREA_CONGEST_MSG._text
         }
         return realTimeDestinationRiskData
     }
