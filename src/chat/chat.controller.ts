3ce7f74c2d09e97aa85ddaf67c2c1cae938689e4
8860k <ysmkk@naver.com>
2024-04-23 02:13:01 +0900

    🎨 delete unused code

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -9,17 +9,11 @@ import { LocationDto } from 'src/users/dto/user-location.dto';
 @ApiTags('chat')
 @Controller('chat')
 export class ChatController {
-  //@@@@@@@@@@@@@@@@@@
   constructor(
     private readonly locationService: LocationDto,
     private readonly chatService: ChatService,
   ) {}
 
-  // @Get()
-  // getChatPage(@Res() res: Response): void {
-  //   const filePath = path.resolve('src', 'chat', 'chat.html'); // 파일 경로 설정 src/chat/chat.html
-
-  //   res.sendFile(filePath);
   @Get('/live-chat')
   @Render('chats/live-chat')
   async liveChat() {}
