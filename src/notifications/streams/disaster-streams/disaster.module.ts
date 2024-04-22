0084eb8f017de3202ac97fef123414e953d26938
unknown <mgeun97@naver.com>
2024-04-22 13:16:38 +0900

    :heavy_plus_sign: : disaster.module : add dependency

--- src/notifications/streams/disaster-streams/disaster.module.ts
+++ src/notifications/streams/disaster-streams/disaster.module.ts
@@ -4,12 +4,14 @@ import { DisasterData } from "src/common/entities/disaster-data.entity";
 import { DisasterService } from "./disaster.service";
 import { DisasterController } from "./disaster.controller";
 import { RedisService } from "src/notifications/redis/redis.service";
+import { HttpModule } from "@nestjs/axios";
+import { NotificationsModule } from "src/notifications/notifications.module";
 
 @Module({
     imports : [
-        TypeOrmModule.forFeature([DisasterData]),
+        TypeOrmModule.forFeature([DisasterData]),HttpModule,NotificationsModule
     ],
     providers : [DisasterService, RedisService],
-    controllers : [DisasterController]
+    controllers : [DisasterController],
 })
 export class DisasterModule {}
