218380e6829dc34dc06b62201c9368111c943e56
ooheunda <ooheunda@gmail.com>
2024-04-24 20:08:23 +0900

    :sparkles: add getPastTime util func

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -6,4 +6,20 @@ export class UtilsService {
   getUUID(): string {
     return uuidv4();
   }
+
+  getPastTime(time: Date): string {
+    const now = new Date();
+    const minDiff = Math.floor((now.getTime() - time.getTime()) / 1000 / 60);
+    if (minDiff < 60) {
+      return `${minDiff}분 전`;
+    } else {
+      const hourDiff = Math.floor(minDiff / 60);
+      if (hourDiff < 24) {
+        return `${hourDiff}시간 전`;
+      } else {
+        const dayDiff = Math.floor(hourDiff / 24);
+        return `${dayDiff}일 전`;
+      }
+    }
+  }
 }
