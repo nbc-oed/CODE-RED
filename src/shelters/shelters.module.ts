f8e44da85f74c0781169e283aa2a5621fba4c99d
unknown <mgeun97@naver.com>
2024-04-26 17:02:47 +0900

    :heavy_plus_sign: : add dependency - shelter.module : ScheduleModule

--- src/shelters/shelters.module.ts
+++ src/shelters/shelters.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Shelters } from 'src/common/entities/shelters.entity';
 import { SheltersService } from './shelters.service';
 import { SheltersController } from './shelters.controller';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shelters])],
+  imports: [TypeOrmModule.forFeature([Shelters]),
+  ScheduleModule.forRoot()],
   providers: [SheltersService],
   controllers: [SheltersController],
   exports: [SheltersService],
